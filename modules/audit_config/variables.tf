de24ac0f094afd7b1d6c3c5866205498de910c5d
Pradeep Ravindran <ravindran@pythian.com>
2020-08-21 12:59:20 -0400

    Update variables.tf

--- modules/audit_config/variables.tf
+++ modules/audit_config/variables.tf
@@ -18,11 +18,10 @@ variable "audit_log_config" {
   description = "List of objects to be added to audit log config"	
   type        = list(object ({ 	
     service: string,	
-    log_config: map(object({	
+    log_config: object({	
       log_type: string,	
       exempted_members: list(string)	
-       })	
-      ) 	
+       }) 	
     })	
   )	
 }
