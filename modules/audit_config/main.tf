5ade4c79e73951eaf327289cfdef5cdaca4fbc22
Pradeep Ravindran <ravindran@pythian.com>
2020-08-21 13:55:29 -0400

    Update main.tf

--- modules/audit_config/main.tf
+++ modules/audit_config/main.tf
@@ -14,19 +14,12 @@
  * limitations under the License.
  */
 
-locals {
-  audit_log_config = {
-    for key, val in var.audit_log_config :
-    key => val
-  }
-}
-
 resource "google_project_iam_audit_config" "project" {
-  for_each = local.audit_log_config
+  for_each = var.audit_log_config
   project  = var.project
   service  = each.value.service
   audit_log_config {
-    log_type         = each.value.log_config.log_type
-    exempted_members = each.value.log_config.exempted_members
+    log_type         = each.value.log_configs.log_type
+    exempted_members = each.value.log_configs.exempted_members
   }
 }
